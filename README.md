126357b4b3b640983517566b6f967cdd36303a2a
lalu <mathieu@digithink.ch>
2016-04-10 15:50:44 +0200

    Better volumes management

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # elk
 A compose file to quickly setup and ELK stack
+
+Try it out with:
+
+    $ docker-machine create -d virtualbox elk
+    $ eval $(docker-machine env elk)
+    $ git clone git@github.com:MBuffenoir/elk.git
+    $ cd elk
+    $ docker-compose up -d
+    $ echo "Hi syslog" | nc -u $(docker-machine ip elk) 5000
--- conf-files/es-conf/indexpattern-init.sh
+++ conf-files/es-conf/indexpattern-init.sh
@@ -12,15 +12,14 @@ curl -XPUT http://localhost:9200/.kibana/index-pattern/\%5Blogstash-%5DYYYY.MM.D
 echo ""
 echo ""
 
-echo "--> Make index pattern [logstash-]YYYY.MM.DD as primary"
-curl -XPUT http://localhost:9200/.kibana/config/4.1.1 -d '{"defaultIndex" : "[logstash-]YYYY.MM.DD"}'
-echo ""
-echo ""
-
-echo "--> Create ES index pattern for : cadvisor"
-curl -XPUT http://localhost:9200/.kibana/index-pattern/cadvisor -d '{"title" : "cadvisor",  "timeFieldName": "container_stats.timestamp"}'
-echo ""
-echo ""
+# echo "--> Make index pattern [logstash-]YYYY.MM.DD as primary"
+# curl -XPUT http://localhost:9200/.kibana/config/4.1.1 -d '{"defaultIndex" : "[logstash-]YYYY.MM.DD"}'
+# echo ""
+# echo ""
+#
+# echo "--> Create ES index pattern for : cadvisor"
+# curl -XPUT http://localhost:9200/.kibana/index-pattern/cadvisor -d '{"title" : "cadvisor",  "timeFieldName": "container_stats.timestamp"}'
+# echo ""
+# echo ""
 
 echo "## END DEPLOY INITIALISATION FOR ELASTICSEARCH #####################################"
-	
